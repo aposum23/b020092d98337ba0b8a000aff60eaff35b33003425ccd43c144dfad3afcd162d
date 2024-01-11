d4c8b93f7e4e05def9bb0c96d28456d4f12fde01
HAXF13D <vovik0312@gmail.com>
2024-01-11 20:27:55 +0300

    изменен комментарий

--- create_db.py
+++ create_db.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
     create_db_query = "CREATE DATABASE IF NOT EXISTS banks_database"
     cur.execute(create_db_query)
 
-    # Запрос для получения списка всех баз данных
+    # Запрос для получения списка всех баз таблиц
     show_db_query = "SHOW DATABASES"
     cur.execute(show_db_query)
     data = cur.fetchall()
